9c0b3dbceab514aee90ff6868bd2734d08a01e55
Kieraaaan <ramseykieran@gmail.com>
2022-05-04 15:09:30 +0100

    Add necessary methods for button functionality

--- multimeter.h
+++ multimeter.h
@@ -12,11 +12,21 @@ void waitForATime(uint32_t howLongToWait);
 int convertADCValue(uint16_t value);
 double convertADCValue2(uint32_t value);
 
+// Configures ADC to read input voltage
 void AdcConfig(void);
+// Configures DAC to output voltage
 void DacConfig(void);
+// Configures pins to be set to logic high or low
 void PinConfig(void);
+// Configures input registers
+void InpConfig(void);
 
+// Main program method
 void setup(void);
+
+void updatePinState(int pin, int state);
+
+int isButtonPressed();
 
 double getVoltage(int scale);
 double getVoltage10(void);
